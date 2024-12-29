b02d3b2bcb52de36eceda0af1cd17f60f053177d
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-12-29 18:59:21 +0545

    update

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
+import fileUpload from "express-fileupload";
+import { endedAuctionCron } from "./automation/endedAuctionCron.js";
+import { verifyCommissionCron } from "./automation/verifyCommissionCron.js";
+import { connection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import auctionItemRouter from "./router/auctionItemRoutes.js";
+import bidRouter from "./router/bidRoutes.js";
+import commissionRouter from "./router/commissionRouter.js";
+import superAdminRouter from "./router/superAdminRoutes.js";
+import userRouter from "./router/userRoutes.js";
+
+const app = express();
+config({
+  path: "./config/config.env",
+});
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auctionitem", auctionItemRouter);
+app.use("/api/v1/bid", bidRouter);
+app.use("/api/v1/commission", commissionRouter);
+app.use("/api/v1/superadmin", superAdminRouter);
+
+endedAuctionCron();
+verifyCommissionCron();
+connection();
+app.use(errorMiddleware);
+
+export default app;
